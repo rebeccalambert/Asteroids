ffb65090578bb2200323315fff2730780210d870
rebeccalambert <rebeccalambert@berkeley.edu>
2019-08-20 11:53:35 -0700

    Finished Asteroid class

--- asteroids/dist/main.js
+++ asteroids/dist/main.js
@@ -91,9 +91,9 @@
   !*** ./src/asteroid.js ***!
   \*************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-eval("\n\nUtil.inherits(Asteroid, MovingObject);\n\nfunction Asteroid (pos) {\n  this.COLOR = \"lightgrey\";\n  this.RADIUS = 15;\n  this.RANDOMVECTOR = Util.randomVec(Math.random(10));\n  MovingObject.call(this,{pos, vel: this.RANDOMVECTOR, color: this.COLOR, radius: this.RADIUS});\n}\n\nmodule.exports = Asteroid;\n\n//# sourceURL=webpack:///./src/asteroid.js?");
+eval("const Util = __webpack_require__(/*! ./util.js */ \"./src/util.js\");\nconst MovingObject = __webpack_require__(/*! ./moving_object.js */ \"./src/moving_object.js\");\n\nUtil.inherits(Asteroid, MovingObject);\n\nfunction Asteroid (pos) {\n  this.COLOR = \"lightgrey\";\n  this.RADIUS = 15;\n  this.RANDOMVECTOR = Util.randomVec(Math.random()*4);\n  MovingObject.call(this,{pos: pos.pos, vel: this.RANDOMVECTOR, color: this.COLOR, radius: this.RADIUS});\n}\n\nmodule.exports = Asteroid;\n\n//# sourceURL=webpack:///./src/asteroid.js?");
 
 /***/ }),
 
@@ -116,6 +116,17 @@ eval("const MovingObject = __webpack_require__(/*! ./moving_object.js */ \"./src
 /***/ (function(module, exports) {
 
 eval("\nfunction MovingObject(options) {\n  this.pos = options.pos;\n  this.vel = options.vel;\n  this.radius = options.radius;\n  this.color = options.color;\n}\n\nMovingObject.prototype.draw = function(ctx) {\n  ctx.beginPath();\n  ctx.arc(this.pos[0], this.pos[1], this.radius, 0, Math.PI*2);\n  ctx.strokeStyle = this.color;\n  ctx.lineWidth = 1;\n  ctx.stroke();\n  ctx.fillStyle = this.color;\n  ctx.fill();\n};\n\nMovingObject.prototype.move = function () {\n  this.pos[0] += this.vel[0];\n  this.pos[1] += this.vel[1];\n};\n\nmodule.exports = MovingObject;\n\n//# sourceURL=webpack:///./src/moving_object.js?");
+
+/***/ }),
+
+/***/ "./src/util.js":
+/*!*********************!*\
+  !*** ./src/util.js ***!
+  \*********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("\nconst Util = {\n  inherits(childClass, parentClass) {\n    function Surrogate(){}\n    Surrogate.prototype = parentClass.prototype;\n    childClass.prototype = new Surrogate();\n    childClass.prototype.constructor = childClass;\n  },\n\n\n  randomVec(length) {\n    const deg = 2 * Math.PI * Math.random();\n    return Util.scale([Math.sin(deg), Math.cos(deg)], length);\n  },\n  scale(vec, m) {\n    return [vec[0] * m, vec[1] * m];\n  }\n};\n\nmodule.exports = Util;\n\n//# sourceURL=webpack:///./src/util.js?");
 
 /***/ })
 
--- asteroids/src/asteroid.js
+++ asteroids/src/asteroid.js
@@ -1,12 +1,13 @@
-
+const Util = require("./util.js");
+const MovingObject = require("./moving_object.js");
 
 Util.inherits(Asteroid, MovingObject);
 
 function Asteroid (pos) {
   this.COLOR = "lightgrey";
   this.RADIUS = 15;
-  this.RANDOMVECTOR = Util.randomVec(Math.random(10));
-  MovingObject.call(this,{pos, vel: this.RANDOMVECTOR, color: this.COLOR, radius: this.RADIUS});
+  this.RANDOMVECTOR = Util.randomVec(Math.random()*4);
+  MovingObject.call(this,{pos: pos.pos, vel: this.RANDOMVECTOR, color: this.COLOR, radius: this.RADIUS});
 }
 
 module.exports = Asteroid;
